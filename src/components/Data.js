6b64af13e6f0d1d00e3f1d7e5287ec22a43428e6
siyathokozaM <siya.mhlawuli13@gmail.com>
2023-10-20 18:55:06 +0200

    data

---
+++ src/components/Data.js
@@ -0,0 +1,173 @@
+export default [
+  {
+    id: 1,
+    productName: "Apple Watch",
+    color: "Black",
+    series: "Series 5 SE",
+    price: 529.99,
+    rating: "4,5/5",
+    info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis pellentesque tellus imperdiet mattis. Proin in quis ipsum non amet imperdiet. Dignissim nisi leo a at. Sit nec lacus, nunc volutpat, tincidunt lorem mi duis. Vitae elementum libero.",
+    description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+       Dignissim odio faucibus nec malesuada purus volutpat vel
+       sed viverra. Id sagittis, phasellus dui in arcu. Nec arcu, 
+       sit nunc, nibh purus pellentesque sagittis. Felis rhoncus 
+       facilisis massa eget purus in purus. Etiam at cras nulla nunc. 
+       Malesuada in pretium diam scelerisque sit mattis in egestas neque. 
+       Eu porta tempor sodales nisl integer turpis porttitor sed sed.
+       Ut senectus odio dictum enim velit tempor diam quisque suspendisse.`,
+    imageUrl: "../images/Watch.png",
+  },
+  {
+    id: 2,
+    productName: "Sony ZX33OBT",
+    color: "Light Grey",
+    price: 39.99,
+    rating: "4,5/5",
+    info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis pellentesque tellus imperdiet mattis. Proin in quis ipsum non amet imperdiet. Dignissim nisi leo a at. Sit nec lacus, nunc volutpat, tincidunt lorem mi duis. Vitae elementum libero.",
+    description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+       Dignissim odio faucibus nec malesuada purus volutpat vel
+       sed viverra. Id sagittis, phasellus dui in arcu. Nec arcu, 
+       sit nunc, nibh purus pellentesque sagittis. Felis rhoncus 
+       facilisis massa eget purus in purus. Etiam at cras nulla nunc. 
+       Malesuada in pretium diam scelerisque sit mattis in egestas neque. 
+       Eu porta tempor sodales nisl integer turpis porttitor sed sed.
+       Ut senectus odio dictum enim velit tempor diam quisque suspendisse.`,
+    imageUrl: "../images/Headphones.png",
+  },
+  {
+    id: 3,
+    productName: "Iphone 12",
+    color: "Black",
+    price: 619.99,
+    rating: "4,5/5",
+    info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis pellentesque tellus imperdiet mattis. Proin in quis ipsum non amet imperdiet. Dignissim nisi leo a at. Sit nec lacus, nunc volutpat, tincidunt lorem mi duis. Vitae elementum libero.",
+    description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+       Dignissim odio faucibus nec malesuada purus volutpat vel
+       sed viverra. Id sagittis, phasellus dui in arcu. Nec arcu, 
+       sit nunc, nibh purus pellentesque sagittis. Felis rhoncus 
+       facilisis massa eget purus in purus. Etiam at cras nulla nunc. 
+       Malesuada in pretium diam scelerisque sit mattis in egestas neque. 
+       Eu porta tempor sodales nisl integer turpis porttitor sed sed.
+       Ut senectus odio dictum enim velit tempor diam quisque suspendisse.`,
+    imageUrl: "../images/Iphone12-01.png",
+  },
+  {
+    id: 4,
+    productName: "Iphone 13",
+    color: "Subway Blue",
+    price: 619.99,
+    rating: "4,5/5",
+    info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis pellentesque tellus imperdiet mattis. Proin in quis ipsum non amet imperdiet. Dignissim nisi leo a at. Sit nec lacus, nunc volutpat, tincidunt lorem mi duis. Vitae elementum libero.",
+    description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+       Dignissim odio faucibus nec malesuada purus volutpat vel
+       sed viverra. Id sagittis, phasellus dui in arcu. Nec arcu, 
+       sit nunc, nibh purus pellentesque sagittis. Felis rhoncus 
+       facilisis massa eget purus in purus. Etiam at cras nulla nunc. 
+       Malesuada in pretium diam scelerisque sit mattis in egestas neque. 
+       Eu porta tempor sodales nisl integer turpis porttitor sed sed.
+       Ut senectus odio dictum enim velit tempor diam quisque suspendisse.`,
+    imageUrl: "../images/Iphone13-Pro-01.png",
+  },
+  {
+    id: 5,
+    productName: "Iphone 12",
+    color: "Product Red",
+    price: 619.99,
+    rating: "4,5/5",
+    info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis pellentesque tellus imperdiet mattis. Proin in quis ipsum non amet imperdiet. Dignissim nisi leo a at. Sit nec lacus, nunc volutpat, tincidunt lorem mi duis. Vitae elementum libero.",
+    description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+       Dignissim odio faucibus nec malesuada purus volutpat vel
+       sed viverra. Id sagittis, phasellus dui in arcu. Nec arcu, 
+       sit nunc, nibh purus pellentesque sagittis. Felis rhoncus 
+       facilisis massa eget purus in purus. Etiam at cras nulla nunc. 
+       Malesuada in pretium diam scelerisque sit mattis in egestas neque. 
+       Eu porta tempor sodales nisl integer turpis porttitor sed sed.
+       Ut senectus odio dictum enim velit tempor diam quisque suspendisse.`,
+    imageUrl: "../images/Iphone12-03.png",
+  },
+  {
+    id: 6,
+    productName: "Iphone 12",
+    color: "Milky White",
+    price: 619.99,
+    rating: "4,5/5",
+    info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis pellentesque tellus imperdiet mattis. Proin in quis ipsum non amet imperdiet. Dignissim nisi leo a at. Sit nec lacus, nunc volutpat, tincidunt lorem mi duis. Vitae elementum libero.",
+    description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+       Dignissim odio faucibus nec malesuada purus volutpat vel
+       sed viverra. Id sagittis, phasellus dui in arcu. Nec arcu, 
+       sit nunc, nibh purus pellentesque sagittis. Felis rhoncus 
+       facilisis massa eget purus in purus. Etiam at cras nulla nunc. 
+       Malesuada in pretium diam scelerisque sit mattis in egestas neque. 
+       Eu porta tempor sodales nisl integer turpis porttitor sed sed.
+       Ut senectus odio dictum enim velit tempor diam quisque suspendisse.`,
+    imageUrl: "../images/Iphone12-04.png",
+  },
+  {
+    id: 7,
+    productName: "Iphone 13",
+    color: "Prduct red",
+    price: 619.99,
+    rating: "4,5/5",
+    info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis pellentesque tellus imperdiet mattis. Proin in quis ipsum non amet imperdiet. Dignissim nisi leo a at. Sit nec lacus, nunc volutpat, tincidunt lorem mi duis. Vitae elementum libero.",
+    description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+       Dignissim odio faucibus nec malesuada purus volutpat vel
+       sed viverra. Id sagittis, phasellus dui in arcu. Nec arcu, 
+       sit nunc, nibh purus pellentesque sagittis. Felis rhoncus 
+       facilisis massa eget purus in purus. Etiam at cras nulla nunc. 
+       Malesuada in pretium diam scelerisque sit mattis in egestas neque. 
+       Eu porta tempor sodales nisl integer turpis porttitor sed sed.
+       Ut senectus odio dictum enim velit tempor diam quisque suspendisse.`,
+    imageUrl: "../images/Iphone12-03.png",
+  },
+  {
+    id: 8,
+    productName: "Iphone 14",
+    color: "Product Red",
+    price: 619.99,
+    rating: "4,5/5",
+    info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis pellentesque tellus imperdiet mattis. Proin in quis ipsum non amet imperdiet. Dignissim nisi leo a at. Sit nec lacus, nunc volutpat, tincidunt lorem mi duis. Vitae elementum libero.",
+    description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+       Dignissim odio faucibus nec malesuada purus volutpat vel
+       sed viverra. Id sagittis, phasellus dui in arcu. Nec arcu, 
+       sit nunc, nibh purus pellentesque sagittis. Felis rhoncus 
+       facilisis massa eget purus in purus. Etiam at cras nulla nunc. 
+       Malesuada in pretium diam scelerisque sit mattis in egestas neque. 
+       Eu porta tempor sodales nisl integer turpis porttitor sed sed.
+       Ut senectus odio dictum enim velit tempor diam quisque suspendisse.`,
+    imageUrl: "../images/Iphone1-03.png",
+  },
+  {
+    id: 9,
+    productName: "Iphone 11",
+    color: "Navy Blue",
+    price: 619.99,
+    rating: "4,5/5",
+    info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis pellentesque tellus imperdiet mattis. Proin in quis ipsum non amet imperdiet. Dignissim nisi leo a at. Sit nec lacus, nunc volutpat, tincidunt lorem mi duis. Vitae elementum libero.",
+    description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+       Dignissim odio faucibus nec malesuada purus volutpat vel
+       sed viverra. Id sagittis, phasellus dui in arcu. Nec arcu, 
+       sit nunc, nibh purus pellentesque sagittis. Felis rhoncus 
+       facilisis massa eget purus in purus. Etiam at cras nulla nunc. 
+       Malesuada in pretium diam scelerisque sit mattis in egestas neque. 
+       Eu porta tempor sodales nisl integer turpis porttitor sed sed.
+       Ut senectus odio dictum enim velit tempor diam quisque suspendisse.`,
+    imageUrl: "../images/Iphone12-Pro-02.png",
+  },
+  {
+    id: 10,
+    productName: "Dell XPS 13",
+    color: "White",
+    price: 1799.99,
+    rating: "4,5/5",
+    info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis pellentesque tellus imperdiet mattis. Proin in quis ipsum non amet imperdiet. Dignissim nisi leo a at. Sit nec lacus, nunc volutpat, tincidunt lorem mi duis. Vitae elementum libero.",
+    description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+       Dignissim odio faucibus nec malesuada purus volutpat vel
+       sed viverra. Id sagittis, phasellus dui in arcu. Nec arcu, 
+       sit nunc, nibh purus pellentesque sagittis. Felis rhoncus 
+       facilisis massa eget purus in purus. Etiam at cras nulla nunc. 
+       Malesuada in pretium diam scelerisque sit mattis in egestas neque. 
+       Eu porta tempor sodales nisl integer turpis porttitor sed sed.
+       Ut senectus odio dictum enim velit tempor diam quisque suspendisse.`,
+    imageUrl: "../images/Dell XPS 13-White.png",
+  },
+];
