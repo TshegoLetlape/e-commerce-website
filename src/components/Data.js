ef1caeed730b1f69d1bad174bde70632acba188a
TshegoLetlape <136719938+TshegoLetlape@users.noreply.github.com>
2023-10-21 17:38:48 +0200

    itempages and item descriptions

--- src/components/Data.js
+++ src/components/Data.js
@@ -6,15 +6,12 @@ export default [
     series: "Series 5 SE",
     price: 529.99,
     rating: "4,5/5",
-    info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis pellentesque tellus imperdiet mattis. Proin in quis ipsum non amet imperdiet. Dignissim nisi leo a at. Sit nec lacus, nunc volutpat, tincidunt lorem mi duis. Vitae elementum libero.",
-    description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-       Dignissim odio faucibus nec malesuada purus volutpat vel
-       sed viverra. Id sagittis, phasellus dui in arcu. Nec arcu, 
-       sit nunc, nibh purus pellentesque sagittis. Felis rhoncus 
-       facilisis massa eget purus in purus. Etiam at cras nulla nunc. 
-       Malesuada in pretium diam scelerisque sit mattis in egestas neque. 
-       Eu porta tempor sodales nisl integer turpis porttitor sed sed.
-       Ut senectus odio dictum enim velit tempor diam quisque suspendisse.`,
+    info: "The Apple Watch Series 5 SE in black is a smartwatch designed and manufactured by Apple Inc.",
+    description: `The Apple Watch Series 5 SE in Black is a sleek and stylish smartwatch that combines fashion 
+    and technology seamlessly. With its stunning OLED display, you can easily view notifications, track your fitness
+    activities, and even make calls right from your wrist. The Series 5 SE offers several advanced features such as
+    heart rate monitoring, GPS tracking, sleep tracking, and support for a wide range of apps from the Apple App Store.
+    Its water-resistant design allows you to wear it during workouts and even swim with it.`,
     imageUrl: "../images/Watch.png",
   },
   {
@@ -23,15 +20,14 @@ export default [
     color: "Light Grey",
     price: 39.99,
     rating: "4,5/5",
-    info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis pellentesque tellus imperdiet mattis. Proin in quis ipsum non amet imperdiet. Dignissim nisi leo a at. Sit nec lacus, nunc volutpat, tincidunt lorem mi duis. Vitae elementum libero.",
-    description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-       Dignissim odio faucibus nec malesuada purus volutpat vel
-       sed viverra. Id sagittis, phasellus dui in arcu. Nec arcu, 
-       sit nunc, nibh purus pellentesque sagittis. Felis rhoncus 
-       facilisis massa eget purus in purus. Etiam at cras nulla nunc. 
-       Malesuada in pretium diam scelerisque sit mattis in egestas neque. 
-       Eu porta tempor sodales nisl integer turpis porttitor sed sed.
-       Ut senectus odio dictum enim velit tempor diam quisque suspendisse.`,
+    info: "The Sony ZX330BT headphones are a pair of wireless Bluetooth headphones known for their comfortable design and excellent sound quality.",
+    description: `The Sony ZX330BT headphones in Light Grey are a stylish and practical audio 
+        solution for music lovers and on-the-go individuals. These over-ear headphones provide an immersive
+        listening experience, thanks to their high-quality sound output.
+        Key features of the Sony ZX330BT headphones include wireless Bluetooth connectivity, which allows
+        you to pair them with your compatible devices, such as smartphones, tablets, or laptops, for a cable-free
+        listening experience. The built-in rechargeable battery offers hours of playtime on a single charge, making
+        them ideal for long commutes or extended listening sessions.`,
     imageUrl: "../images/Headphones.png",
   },
   {
@@ -40,15 +36,13 @@ export default [
     color: "Black",
     price: 619.99,
     rating: "4,5/5",
-    info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis pellentesque tellus imperdiet mattis. Proin in quis ipsum non amet imperdiet. Dignissim nisi leo a at. Sit nec lacus, nunc volutpat, tincidunt lorem mi duis. Vitae elementum libero.",
-    description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-       Dignissim odio faucibus nec malesuada purus volutpat vel
-       sed viverra. Id sagittis, phasellus dui in arcu. Nec arcu, 
-       sit nunc, nibh purus pellentesque sagittis. Felis rhoncus 
-       facilisis massa eget purus in purus. Etiam at cras nulla nunc. 
-       Malesuada in pretium diam scelerisque sit mattis in egestas neque. 
-       Eu porta tempor sodales nisl integer turpis porttitor sed sed.
-       Ut senectus odio dictum enim velit tempor diam quisque suspendisse.`,
+    info: "The iPhone 12 is a flagship smartphone manufactured by Apple Inc., known for its powerful performance, sleek design, and innovative features.",
+    description: `The iPhone 12 in Black is a stunning piece of technology that exemplifies Apple's 
+    commitment to quality and innovation. This smartphone not only offers a seamless user experience but
+    also packs a range of features that make it one of the top choices in the mobile device market.The
+    iPhone 12 features a beautiful Super Retina XDR display that offers vibrant colors and deep blacks,
+    making it perfect for watching videos, browsing the web, and enjoying your favorite apps. Its A14
+    Bionic chip ensures speedy performance and enables you to run the latest apps and games without a hitch.`,
     imageUrl: "../images/Iphone12-01.png",
   },
   {
@@ -57,15 +51,13 @@ export default [
     color: "Subway Blue",
     price: 619.99,
     rating: "4,5/5",
-    info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis pellentesque tellus imperdiet mattis. Proin in quis ipsum non amet imperdiet. Dignissim nisi leo a at. Sit nec lacus, nunc volutpat, tincidunt lorem mi duis. Vitae elementum libero.",
-    description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-       Dignissim odio faucibus nec malesuada purus volutpat vel
-       sed viverra. Id sagittis, phasellus dui in arcu. Nec arcu, 
-       sit nunc, nibh purus pellentesque sagittis. Felis rhoncus 
-       facilisis massa eget purus in purus. Etiam at cras nulla nunc. 
-       Malesuada in pretium diam scelerisque sit mattis in egestas neque. 
-       Eu porta tempor sodales nisl integer turpis porttitor sed sed.
-       Ut senectus odio dictum enim velit tempor diam quisque suspendisse.`,
+    info: "The iPhone 13 in Subway Blue represents the latest evolution of Apple's iconic smartphone line. With its striking color, top-tier performance, and innovative features, it continues to set the standard for modern smartphones.",
+    description: `The iPhone 13 boasts a powerful A15 Bionic chip, ensuring fast and efficient performance, 
+    whether you're multitasking, gaming, or using resource-intensive applications. The Super Retina XDR display
+     delivers stunning visuals, vibrant colors, and deep blacks, making it a pleasure to use for all your
+     entertainment and productivity needs. One of the standout features of the iPhone 13 is its improved battery
+     life, allowing you to go longer between charges. And, with 5G support, you can enjoy lightning-fast internet
+      speeds, ensuring smooth streaming and quick downloads. `,
     imageUrl: "../images/Iphone13-Pro-01.png",
   },
   {
@@ -74,15 +66,13 @@ export default [
     color: "Product Red",
     price: 619.99,
     rating: "4,5/5",
-    info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis pellentesque tellus imperdiet mattis. Proin in quis ipsum non amet imperdiet. Dignissim nisi leo a at. Sit nec lacus, nunc volutpat, tincidunt lorem mi duis. Vitae elementum libero.",
-    description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-       Dignissim odio faucibus nec malesuada purus volutpat vel
-       sed viverra. Id sagittis, phasellus dui in arcu. Nec arcu, 
-       sit nunc, nibh purus pellentesque sagittis. Felis rhoncus 
-       facilisis massa eget purus in purus. Etiam at cras nulla nunc. 
-       Malesuada in pretium diam scelerisque sit mattis in egestas neque. 
-       Eu porta tempor sodales nisl integer turpis porttitor sed sed.
-       Ut senectus odio dictum enim velit tempor diam quisque suspendisse.`,
+    info: "The iPhone 12 is a flagship smartphone manufactured by Apple Inc., known for its powerful performance, sleek design, and innovative features.",
+    description: `The iPhone 12 in Product Red is a stunning piece of technology that exemplifies Apple's 
+    commitment to quality and innovation. This smartphone not only offers a seamless user experience but
+    also packs a range of features that make it one of the top choices in the mobile device market.The
+    iPhone 12 features a beautiful Super Retina XDR display that offers vibrant colors and deep blacks,
+    making it perfect for watching videos, browsing the web, and enjoying your favorite apps. Its A14
+    Bionic chip ensures speedy performance and enables you to run the latest apps and games without a hitch.`,
     imageUrl: "../images/Iphone12-03.png",
   },
   {
@@ -91,32 +81,28 @@ export default [
     color: "Milky White",
     price: 619.99,
     rating: "4,5/5",
-    info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis pellentesque tellus imperdiet mattis. Proin in quis ipsum non amet imperdiet. Dignissim nisi leo a at. Sit nec lacus, nunc volutpat, tincidunt lorem mi duis. Vitae elementum libero.",
-    description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-       Dignissim odio faucibus nec malesuada purus volutpat vel
-       sed viverra. Id sagittis, phasellus dui in arcu. Nec arcu, 
-       sit nunc, nibh purus pellentesque sagittis. Felis rhoncus 
-       facilisis massa eget purus in purus. Etiam at cras nulla nunc. 
-       Malesuada in pretium diam scelerisque sit mattis in egestas neque. 
-       Eu porta tempor sodales nisl integer turpis porttitor sed sed.
-       Ut senectus odio dictum enim velit tempor diam quisque suspendisse.`,
+    info: "The iPhone 12 is a flagship smartphone manufactured by Apple Inc., known for its powerful performance, sleek design, and innovative features.",
+    description: `The iPhone 12 in Milky White is a stunning piece of technology that exemplifies Apple's 
+    commitment to quality and innovation. This smartphone not only offers a seamless user experience but
+    also packs a range of features that make it one of the top choices in the mobile device market.The
+    iPhone 12 features a beautiful Super Retina XDR display that offers vibrant colors and deep blacks,
+    making it perfect for watching videos, browsing the web, and enjoying your favorite apps. Its A14
+    Bionic chip ensures speedy performance and enables you to run the latest apps and games without a hitch.`,
     imageUrl: "../images/Iphone12-04.png",
   },
   {
     id: 7,
     productName: "Iphone 13",
-    color: "Prduct red",
+    color: "Product red",
     price: 619.99,
     rating: "4,5/5",
-    info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis pellentesque tellus imperdiet mattis. Proin in quis ipsum non amet imperdiet. Dignissim nisi leo a at. Sit nec lacus, nunc volutpat, tincidunt lorem mi duis. Vitae elementum libero.",
-    description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-       Dignissim odio faucibus nec malesuada purus volutpat vel
-       sed viverra. Id sagittis, phasellus dui in arcu. Nec arcu, 
-       sit nunc, nibh purus pellentesque sagittis. Felis rhoncus 
-       facilisis massa eget purus in purus. Etiam at cras nulla nunc. 
-       Malesuada in pretium diam scelerisque sit mattis in egestas neque. 
-       Eu porta tempor sodales nisl integer turpis porttitor sed sed.
-       Ut senectus odio dictum enim velit tempor diam quisque suspendisse.`,
+    info: "The iPhone 13 in Product Red is a remarkable addition to Apple's smartphone lineup. Beyond its stunning and vibrant red hue, this device is packed with features and capabilities that make it a standout choice for tech enthusiasts and style-conscious individuals alike.",
+    description: `The iPhone 13 boasts a powerful A15 Bionic chip, ensuring fast and efficient performance, 
+    whether you're multitasking, gaming, or using resource-intensive applications. The Super Retina XDR display
+     delivers stunning visuals, vibrant colors, and deep blacks, making it a pleasure to use for all your
+     entertainment and productivity needs. One of the standout features of the iPhone 13 is its improved battery
+     life, allowing you to go longer between charges. And, with 5G support, you can enjoy lightning-fast internet
+      speeds, ensuring smooth streaming and quick downloads.`,
     imageUrl: "../images/Iphone12-03.png",
   },
   {
@@ -125,15 +111,12 @@ export default [
     color: "Product Red",
     price: 619.99,
     rating: "4,5/5",
-    info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis pellentesque tellus imperdiet mattis. Proin in quis ipsum non amet imperdiet. Dignissim nisi leo a at. Sit nec lacus, nunc volutpat, tincidunt lorem mi duis. Vitae elementum libero.",
-    description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-       Dignissim odio faucibus nec malesuada purus volutpat vel
-       sed viverra. Id sagittis, phasellus dui in arcu. Nec arcu, 
-       sit nunc, nibh purus pellentesque sagittis. Felis rhoncus 
-       facilisis massa eget purus in purus. Etiam at cras nulla nunc. 
-       Malesuada in pretium diam scelerisque sit mattis in egestas neque. 
-       Eu porta tempor sodales nisl integer turpis porttitor sed sed.
-       Ut senectus odio dictum enim velit tempor diam quisque suspendisse.`,
+    info: "Phone 14. With the most impressive dual-camera system on iPhone.Capture stunning photos in low light and bright light.",
+    description: `Crash Detection, a new safety feature, calls for help when you can’t.
+    6.1-inch Super Retina XDR display. The iPhone 14 features the popular 6.1-inch display.
+With OLED technology, the Super Retina XDR display has 800 nits peak brightness outdoors and
+delivers 1200 nits peak brightness for HDR photos and movies.
+Advanced camera system for better photos in any light.`,
     imageUrl: "../images/Iphone1-03.png",
   },
   {
@@ -142,15 +125,12 @@ export default [
     color: "Navy Blue",
     price: 619.99,
     rating: "4,5/5",
-    info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis pellentesque tellus imperdiet mattis. Proin in quis ipsum non amet imperdiet. Dignissim nisi leo a at. Sit nec lacus, nunc volutpat, tincidunt lorem mi duis. Vitae elementum libero.",
-    description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-       Dignissim odio faucibus nec malesuada purus volutpat vel
-       sed viverra. Id sagittis, phasellus dui in arcu. Nec arcu, 
-       sit nunc, nibh purus pellentesque sagittis. Felis rhoncus 
-       facilisis massa eget purus in purus. Etiam at cras nulla nunc. 
-       Malesuada in pretium diam scelerisque sit mattis in egestas neque. 
-       Eu porta tempor sodales nisl integer turpis porttitor sed sed.
-       Ut senectus odio dictum enim velit tempor diam quisque suspendisse.`,
+    info: "The iPhone 11 in Navy Blue is a testament to Apple's commitment to providing a wide range of options to its customers. While it may not be the latest model, it remains a popular and capable smartphone that offers an array of features.",
+    description: `One of the highlights of the iPhone 11 is its dual-camera system, which 
+    includes an ultra-wide and wide lens. This allows users to capture stunning photos and videos,
+    from wide-angle landscapes to detailed close-ups. The Night mode feature ensures that your low-light
+    photography shines.The iPhone 11 also offers Face ID for secure and convenient authentication.
+    Its excellent battery life ensures that you can use the device throughout the day without frequent recharging. `,
     imageUrl: "../images/Iphone12-Pro-02.png",
   },
   {
@@ -159,15 +139,12 @@ export default [
     color: "White",
     price: 1799.99,
     rating: "4,5/5",
-    info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis pellentesque tellus imperdiet mattis. Proin in quis ipsum non amet imperdiet. Dignissim nisi leo a at. Sit nec lacus, nunc volutpat, tincidunt lorem mi duis. Vitae elementum libero.",
-    description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-       Dignissim odio faucibus nec malesuada purus volutpat vel
-       sed viverra. Id sagittis, phasellus dui in arcu. Nec arcu, 
-       sit nunc, nibh purus pellentesque sagittis. Felis rhoncus 
-       facilisis massa eget purus in purus. Etiam at cras nulla nunc. 
-       Malesuada in pretium diam scelerisque sit mattis in egestas neque. 
-       Eu porta tempor sodales nisl integer turpis porttitor sed sed.
-       Ut senectus odio dictum enim velit tempor diam quisque suspendisse.`,
+    info: "The Dell XPS 13 in White is a flagship laptop from Dell's renowned XPS series, known for its exceptional build quality, performance, and stunning design.",
+    description: `The XPS 13's battery life is impressive, allowing you to work or stream 
+    content for extended periods without needing to recharge. It also offers a range of connectivity
+    options, including USB-C and Thunderbolt 4 ports for fast data transfer and external device support.
+    Under the hood, the XPS 13 is powered by the latest Intel processors, ensuring snappy performance for
+    both everyday tasks and demanding applications.`,
     imageUrl: "../images/Dell XPS 13-White.png",
   },
 ];
---
+++ src/itempage.js
@@ -0,0 +1,7 @@
+import React from "react";
+import "./components/Data";
+import "./itempage.css";
+
+<div className="container">
+  <button className="back">Back</button>
+</div>;
