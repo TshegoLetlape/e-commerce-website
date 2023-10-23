c55b9844519762642eee66af3730586493803a31
siyathokozaM <siya.mhlawuli13@gmail.com>
2023-10-23 08:43:58 +0200

    home

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,19 +7,19 @@ const Home = () => {
 
   return (
     <div className="container">
-      <div className="text-center m-5">
-        <input className="rounded" type="search" />
-        <button>search</button>
+      <div className="text-center m-5 d-flex justify-content-center">
+        <input className="rounded form-control" type="search" />
+        <button className="search">search</button>
       </div>
       <div className="items justify-content-center">
         {data.map((item) => (
           <ul id={item.id}>
             <li className=" list-item m-3">
               <img src={item.imageUrl} />
-              <h5>{item.productName}</h5>
+              <h5 className="mt-3">{item.productName}</h5>
               <p className="color">{item.color}</p>
-              <p>
-                <span className="price">${item.price}</span>
+              <p className="p">
+                <span className="price">$ {item.price}</span>
                 <button>
                   <i class="fa fa-shopping-bag" aria-hidden="true"></i>
                 </button>
