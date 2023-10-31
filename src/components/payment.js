8505c849e5cca4e30bbbf731c0577319412f212c
TshegoLetlape <136719938+TshegoLetlape@users.noreply.github.com>
2023-10-31 16:01:36 +0200

    payment method page

--- src/components/payment.js
+++ src/components/payment.js
@@ -1,4 +1,6 @@
 import React from "react";
+import "../payment.css";
+import { Link } from "react-router-dom";
 
 function PaymentForm() {
   return (
@@ -53,7 +55,7 @@ function PaymentForm() {
                   <div className="flex-fill mx-3">
                     <div className="form-group" controlId="formControlLgXc">
                       <label className="form-label">Card Number</label>
-                      <input type="text" value="**** **** **** 3193" />
+                      <input type="text" value="**** **** **** 4511" />
                     </div>
                   </div>
                   <a href="#!">Remove card</a>
@@ -67,7 +69,7 @@ function PaymentForm() {
                   <input
                     className="form-control"
                     type="text"
-                    value="Anna Doe"
+                    placeholder="Name On Card"
                   />
                 </form>
 
@@ -79,7 +81,8 @@ function PaymentForm() {
                       <input
                         className="form-control"
                         type="text"
-                        value="1234 5678 1234 5678"
+                        placeholder="Card Number"
+                        maxlength="19"
                       />
                     </form>
                   </div>
@@ -92,6 +95,7 @@ function PaymentForm() {
                         className="form-control"
                         type="text"
                         placeholder="MM/YYYY"
+                        maxlength="5"
                       />
                     </form>
                   </div>
@@ -105,13 +109,40 @@ function PaymentForm() {
                         type="password"
                         placeholder="Cvv"
                       />
+                      <i
+                        className="fa-solid fa-circle-question"
+                        title="3 digits at the back of the card"
+                        cursor="pointer"
+                      ></i>
                     </form>
                   </div>
+                  <p>
+                    <input type="checkbox" className="deflt" />
+                    Save This As Your Default Payment Method
+                  </p>
                 </div>
-
-                <button className="btn btn-dark" variant="dark" size="lg">
-                  Add Payment Method
-                </button>
+                <Link to="/checkout">
+                  <button className="btn btn-dark" variant="dark" size="lg">
+                    <svg
+                      width="24"
+                      height="24"
+                      viewBox="0 0 25 24"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M2 17.625C2 18.3212 2.27656 18.9889 2.76884 19.4812C3.26113 19.9734 3.92881 20.25 4.625 20.25H20.375C21.0712 20.25 21.7389 19.9734 22.2312 19.4812C22.7234 18.9889 23 18.3212 23 17.625V10.4062H2V17.625ZM5.09375 14.0625C5.09375 13.6895 5.24191 13.3319 5.50563 13.0681C5.76935 12.8044 6.12704 12.6562 6.5 12.6562H8.75C9.12296 12.6562 9.48065 12.8044 9.74437 13.0681C10.0081 13.3319 10.1562 13.6895 10.1562 14.0625V15C10.1562 15.373 10.0081 15.7306 9.74437 15.9944C9.48065 16.2581 9.12296 16.4062 8.75 16.4062H6.5C6.12704 16.4062 5.76935 16.2581 5.50563 15.9944C5.24191 15.7306 5.09375 15.373 5.09375 15V14.0625ZM20.375 3.75H4.625C3.92881 3.75 3.26113 4.02656 2.76884 4.51884C2.27656 5.01113 2 5.67881 2 6.375V7.59375H23V6.375C23 5.67881 22.7234 5.01113 22.2312 4.51884C21.7389 4.02656 21.0712 3.75 20.375 3.75Z"
+                        fill="white"
+                      />
+                    </svg>
+                    <div>Add Payment Method</div>
+                  </button>{" "}
+                </Link>
+                <br />
+                <br />
+                <Link to="/checkout" className="back-btn">
+                  Back
+                </Link>
               </form>
             </div>
           </div>
